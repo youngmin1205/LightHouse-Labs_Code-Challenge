99d5c8bc575e22296f5b38f5ab7e0d82895cb5ca
YoungShin Min <youngshinmin1205@gmail.com>
2018-10-24 18:10:56 -0400

    Challenge 5

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const GRID = [
   ["", "^", "", "", "~", "~", "", "", "", ""],
 ];
 
+//challenge
+
 const countRows = function() {
   return GRID.length;
 }
@@ -25,4 +27,25 @@ const gridSize = function() {
 
 const totalCells = function() {
   return countColumns() * countRows();
+}
+
+const COLUMN_CHAR_START = 'A'.charCodeAt(0);
+
+const letterToIndex = (letter) => {
+  return letter.toUpperCase().charCodeAt(0) - COLUMN_CHAR_START;
+};
+
+const getIndices = (coordinate) => {
+  const columnLetter = coordinate.slice(0,1);
+  const rowNumber = coordinate.slice(1);
+
+  return {
+    columnIndex: letterToIndex(columnLetter),
+    rowIndex: rowNumber - 1
+  };
+};
+
+const convertColumn = function(coordinate) {
+  const { columnIndex } = getIndices(coordinate);
+  return columnIndex;
 }
