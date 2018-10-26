0fc72eacd37981b8e779c16de97eeacb10d3565a
YoungShin Min <youngshinmin1205@gmail.com>
2018-10-25 22:42:38 -0400

    Chanllenge 6 & 7

--- index.js
+++ index.js
@@ -12,23 +12,27 @@ const GRID = [
 ];
 
 //challenge
-
+//1
 const countRows = function() {
   return GRID.length;
 }
 
+//2
 const countColumns = function() {
   return GRID[0].length;
 }
 
+//3
 const gridSize = function() {
   return `${countColumns()} x ${countRows()}`;
 }
 
+//4
 const totalCells = function() {
   return countColumns() * countRows();
 }
 
+//5
 const COLUMN_CHAR_START = 'A'.charCodeAt(0);
 
 const letterToIndex = (letter) => {
@@ -49,3 +53,30 @@ const convertColumn = function(coordinate) {
   const { columnIndex } = getIndices(coordinate);
   return columnIndex;
 }
+
+//6
+//GRID[3][2] 'c4'  GRID [][columnIndex]
+function lightCell(coordinates) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split(''); 
+  //string.split(separator, limit)
+  //["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  let cordX = coordinates.split("")[0].toLowerCase();  //e.g A
+  let cordY = coordinates.split("")[1] - 1; // e.g. 3
+  let y = GRID[cordY]; //GRID[3]
+  let x = alphabet.indexOf(cordX); 
+  /*function myFunction() {
+    var str = "Helloworld,welcome to the universe.";
+    var n = str.indexOf("welcome");
+    document.getElementById("demo").innerHTML = n;
+}*/
+  let answer = y[x];
+  return answer;
+}
+
+lightCell('B4'); // " "
+
+//7
+const isRock = function(coordinates){
+  return Boolean(lightCell(coordinate) == '^');
+}
+
